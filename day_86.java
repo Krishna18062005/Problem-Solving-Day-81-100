d4f947891c8c042ea6637848b06ea87eb924866c
Krishna K <krish1826s@gmail.com>
2024-07-18 13:15:00 +0530

    Update day_86.java

--- day_86.java
+++ day_86.java
@@ -24,3 +24,27 @@ public class Hello {
 
 	}
 }
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args) {
+		//Rhyming Words
+		Scanner sc=new Scanner(System.in);
+		String[] str=sc.nextLine().split(" ");
+		int c=0;
+		for(int i=0;i<str.length;i++){
+		    if(str[i].equals("")) continue;
+		    String seq=str[i].substring(str[i].length()/2).toLowerCase();
+		    Boolean cc=false;
+		    for(int j=i+1;j<str.length;j++){
+		        if(str[j].substring(str[j].length()/2).toLowerCase().equals(seq)){
+		         cc=true;   str[j]="";
+		            c++;
+		        }
+		    }
+		   if(cc) c++;
+		}
+		System.out.print(c);
+
+	}
+}
