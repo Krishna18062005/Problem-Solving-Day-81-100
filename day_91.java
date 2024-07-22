a1635f604ca8760afdddb85b42783d05beb26584
Krishna K <krish1826s@gmail.com>
2024-07-22 20:55:28 +0530

    Update day_91.java

--- day_91.java
+++ day_91.java
@@ -17,3 +17,26 @@ class Solution {
         }return names;
     }
 }
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args) {
+		//print Operators
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int[] ar=new int[n];
+		for(int i=0;i<n;i++){
+		    ar[i]=sc.nextInt();
+		}
+		int fn=sc.nextInt();
+		int sn=sc.nextInt();
+		for(int i=0;i<n;i++){
+		    if((int)(fn/sn)==ar[i]) System.out.print("/ ");
+		    else if(fn-sn==ar[i]) System.out.print("- ");
+		    else if(fn*sn==ar[i]) System.out.print("* ");
+		    else if((fn+sn)==ar[i]) System.out.print("+ ");
+		    else System.out.print("# ");
+		}
+
+	}
+}
